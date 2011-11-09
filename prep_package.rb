7a02ac98a8f5a4962d8bf0736fd6af239142e386
Brandon Arp <brandon@arpnetworking.com>
2011-11-09 09:47:31 -0800

    mostly working package prep

--- prep_package.rb
+++ prep_package.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
+require 'fileutils'
+
 def get_package_info(package_name)
   package_info = `apt-cache show #{package_name}`
   puts "error looking up package '#{package_name}'" unless $? == 0
@@ -13,6 +15,14 @@ def get_package_provides(package_info)
   }
 end
 
+def get_package_filename(package_info)
+  package_info.each_line { |line|
+    if (line =~ /Filename: (.*)/)
+      return $1
+    end
+  }
+end
+
 def get_package_version(package_info)
   package_info.each_line { |line|
     if (line =~ /Version: (.*)/)
@@ -21,16 +31,40 @@ def get_package_version(package_info)
   }
 end
 
+def get_package_architecture(package_info)
+  package_info.each_line { |line|
+    if (line =~ /Architecture: (.*)/)
+      return $1
+    end
+  }
+end
+
+def get_package_name(package_info)
+  package_info.each_line { |line|
+    if (line =~ /Package: (.*)/)
+      return $1
+    end
+  }
+end
+
+def get_cache_deb(package_info) 
+  package_name = get_package_name(package_info)
+  package_version = get_package_version(package_info)
+  package_architecture = get_package_architecture(package_info)
+  deb_name = "#{package_name}_#{package_version}_#{package_architecture}"
+  deb_name = deb_name.gsub(":", "%3a")
+  return "/var/cache/apt/archives/#{deb_name}"
+end
+
 def recurse_prep(package_name, recurse_hash = Hash.new(), provides = Hash.new())
   package_depends = `apt-cache depends #{package_name}`
   in_or_block = false
   package_depends.each_line { |line|
     if (line =~ /Depends:\s<?([^>]+)\s*/)
       dependency = $1.chomp
-#      dependency = $1
       store_dep = false
       if (!in_or_block)
-	store_dep = true
+        store_dep = true
       end
       if (line.strip!().start_with?("|"))
         in_or_block = true
@@ -42,11 +76,25 @@ def recurse_prep(package_name, recurse_hash = Hash.new(), provides = Hash.new())
         version = get_package_version(package_info)
 	provides_list = get_package_provides(package_info)
         provides_list.each{|provide| provides[provide] = dependency}
-        puts "#{dependency}=>#{version}  (provides #{provides.length} packages)"
+        puts "#{dependency}=>#{version}  (provides #{provides_list.length} packages)"
         recurse_prep(dependency, recurse_hash, provides)
       end 
     end
   }
+  #install me
+  my_info = get_package_info(package_name)
+  my_version = get_package_version(my_info)
+  my_cache_deb = get_cache_deb(my_info)
+  package_loc = "/chimera/packages/#{package_name}/#{my_version}"
+  if not File.directory?(package_loc)
+    FileUtils.mkpath(package_loc)
+  end
+
+  if not File.exist?(my_cache_deb)
+    `apt-get install -y -d --reinstall #{package_name}`
+  end
+  puts "extracting #{my_cache_deb} to #{package_loc}"
+  `dpkg -x #{my_cache_deb} #{package_loc}`
 end
 
 package_name = ARGV[0]
