c15fbd8733a49fa5a76e728e5648b690667bb607
Brandon Arp <brandon@arpnetworking.com>
2011-11-21 13:04:36 -0800

    fixed some bugs with set_env_status

--- set_env_status.rb
+++ set_env_status.rb
@@ -1,32 +1,45 @@
 #!/usr/bin/env ruby
 require 'optparse'
+require 'etc'
 
 def execute_scripts(admin_dir, standard_dir)
   if (File.exist?(admin_dir))
     admin_scripts = Dir.entries(admin_dir)
+    admin_scripts.delete(".")
+    admin_scripts.delete("..")
   else 
     puts "no admin scripts found"
   end
 
   if (File.exist?(standard_dir))
     reg_scripts = Dir.entries(standard_dir)
+    reg_scripts.delete(".")
+    reg_scripts.delete("..")
   else 
     puts "no standard scripts found"
   end
   all_scripts = []
-  all_scripts = admin_scripts.map{|item| [item, true]} if admin_scripts != nil
-  all_script = all_scripts + reg_scripts.map{|item| [item, false]} if reg_scripts != nil
+  all_scripts = admin_scripts.map{|item| [admin_dir + "/" + item, true]} if admin_scripts != nil
+  all_script = all_scripts + reg_scripts.map{|item| [standard_dir + "/" + item, false]} if reg_scripts != nil
   all_scripts.sort!{|a,b| a[0]<=>b[0]}
-  for script in all_scripts do
+  for script_entry in all_scripts do
+    script = script_entry[0]
+    as_admin = script_entry[1]
     other = fork()
     if (other != nil)
       puts "Waiting for script #{script} to complete"
       Process.waitpid(other)
     else
-      puts "would be execing #{admin}"
-      sleep 2
+      cmd = "/chimera/bin/env -e #{$ENV_NAME} #{script}"
+      puts "would be execing #{cmd} as admin" if as_admin
+      puts "would be execing #{cmd}" if as_admin == false
+      if as_admin == false
+        uid = Etc.getpwnam("nobody").uid
+        Process::Sys.setuid(uid)
+      end
+      Kernel.exec(cmd)
+      exit!
     end
-    puts "activate: #{admin}"
   end
 end
 
@@ -63,6 +76,7 @@ if (env_name == nil)
   puts "no environment to act on"
   exit!
 end
+$ENV_NAME = env_name
 command_root = "/chimera/env/" + env_name + "/usr/lib/chimera/command"
 preactivate_admin = command_root + "/preactivateadmin"
 preactivate = command_root + "/preactivate"
